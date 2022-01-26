1253206a24515aa21d8554647b3f357690c23f30
Spencer-Lake <spencer.m.lake11@gmail.com>
2022-01-26 15:53:18 -0500

    added state population pages

--- client/index.js
+++ client/index.js
@@ -1,5 +1,3 @@
-let yearSelection = ''
-
 // function that calls API and creates HTML content using data
 $(function(e) {
     e.preventDefault
@@ -11,40 +9,12 @@ $(function(e) {
         type: 'GET',
         url: 'https://datausa.io/api/data?drilldowns=Nation&measures=Population',
         success: function(data) {
+            data.preventDefault
             // Function that creating the content for each data set returned from API
             $.each(data.data, function(i, stuff) {
                 // HTML formatting for content
-                // $cards.append('<a class="card-holder" id="'+ stuff.Year +'" href="states.html" onClick="selectingYear('+stuff.Year+')"><div class="card"><p class="card-year"> '+ stuff.Year +'</p><p class="card-population">'+ stuff.Population +'<p></div></a>')
-                $cards.append('<a class="card-holder" id="'+ stuff.Year +'" href="states.html" (click)="$event.preventDefault()" onClick="selectingYear('+stuff.Year+')"><div class="card"><p class="card-year"> '+ stuff.Year +'</p><p class="card-population">'+ stuff.Population +'<p></div></a>')
+                $cards.append('<a class="card-holder" id="'+ stuff.Year +'" href="'+ stuff.Year +'.html"><div class="card"><p class="card-year"> '+ stuff.Year +'</p><p class="card-population">'+ stuff.Population +'<p></div></a>')
             })
         }
     })
 })
-
-function selectingYear(year) {
-    year.preventDefault
-    yearSelection = year
-    console.log(yearSelection)
-}
-
-// Function for displaying population of each state during the given year
-$(function(e) {
-    e.preventDefault
-    // Setting location where HTML content will be created (states page)
-    const $stateCards = $('#cards-container-states')
-
-    // Ajax request to API for getting state information
-    $.ajax({
-        type: 'GET',
-        url: `https://datausa.io/api/data?drilldowns=State&measures=Population&year=${yearSelection}`,
-        success: function(data) {
-            // console.log(yearSelection)
-            // console.log(data.data)
-            $.each(data.data, function(i, stuff) {
-                // console.log(stuff)
-                $stateCards.append('<div class="card"><p class="card-year"> '+ stuff.State +'</p><p class="card-population">'+ stuff.Population +'<p></div>')
-            })
-        }
-    })
-})
-
---
+++ server/2019.js
@@ -0,0 +1,20 @@
+// Function for displaying population of each state during the given year
+$(function () {
+    
+    const $stateCards = $('#cards-container-states')
+
+    // Ajax request to API for getting state information
+    $.ajax({
+        type: 'GET',
+        url: 'https://datausa.io/api/data?drilldowns=State&measures=Population&year=2019',
+        success: function(data) {
+            data.preventDefault
+            console.log(data.data)
+            $.each(data.data, function(i, stuff) {
+                stuff.preventDefault
+                console.log(stuff)
+                $stateCards.append('<div class="card"><p class="card-year"> '+ stuff.State +'</p><p class="card-population">'+ stuff.Population +'<p></div>')
+            })
+        }
+    })
+})
