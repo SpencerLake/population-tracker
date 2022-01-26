25d24015be1b6f8ef99adb234e6b45f023ad41e8
Spencer-Lake <spencer.m.lake11@gmail.com>
2022-01-26 10:07:15 -0500

    added documentation to js file

--- client/index.js
+++ client/index.js
@@ -1,20 +1,20 @@
-console.log('Hello World')
-
+// function that calls API and creates HTML content using data
 $(function (){
 
+    // Setting location where HTML content will be created
     const $cards = $('#cards-container')
 
+    // Ajax request to API
     $.ajax({
         type: 'GET',
         url: 'https://datausa.io/api/data?drilldowns=Nation&measures=Population',
         success: function(data) {
-            console.log(data)
+            // Function that creating the content for each data set returned from API
             $.each(data.data, function(i, stuff) {
+                // HTML formatting for content
                 $cards.append('<div class="card"><p class="card-year"> '+ stuff.Year +'</p><p class="card-population">'+ stuff.Population +'<p></div>')
-                // console.log(stuff.Year)
             })
         }
     })
-
 })
 
