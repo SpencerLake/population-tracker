93ee37b1119ca419b5d8a900850f827f83097c3e
Spencer-Lake <spencer.m.lake11@gmail.com>
2022-01-27 13:04:08 -0500

    fixed bugs

--- server/2018.js
+++ server/2018.js
@@ -1,3 +1,38 @@
+// Function for adding content to the Modal popup
+function popup(state) {
+    
+    newState = state.toLowerCase()
+
+    // Ajax query calling slug information from API and posting it onto popup
+    $(function () {
+
+        $.ajax({
+            type: 'GET',
+            url: 'https://datausa.io/api/profile?slug=geo&id='+ newState +'',
+            success: function(data) {
+
+                const $title = $('#stateModalTitle')
+                const $info = $('#info1')
+
+                // Populating content
+                $title.append(state)
+                $info.append(data.ids)
+            }
+        })
+    })
+}
+
+// Function that makes it so the information on the Modal popup is cleared
+function clearPopup() {
+    const $title = $('#stateModalTitle')
+    const $info = $('#info1') 
+
+    // Clears selected tag
+    $title.empty()
+    $info.empty()
+}
+
+
 // Function for displaying population of each state during the given year
 $(function () {
     
@@ -8,13 +43,14 @@ $(function () {
         type: 'GET',
         url: 'https://datausa.io/api/data?drilldowns=State&measures=Population&year=2018',
         success: function(data) {
-            data.preventDefault
-            console.log(data.data)
-            $.each(data.data, function(i, stuff) {
-                stuff.preventDefault
-                console.log(stuff)
-                $stateCards.append('<div class="card"><p class="card-year"> '+ stuff.State +'</p><p class="card-population">'+ stuff.Population +'<p></div>')
+            $.each(data.data, function(i, stateData) {
+
+                const s = stateData
+                let trimmedState = $.trim(s.State)
+
+                // Formatting for the state/population cards, and includes the function calls
+                $stateCards.append('<div class="card" id="'+ s.State +'"><p class="card-year"> '+ s.State +'</p><p class="card-population">'+ s.Population +'<p><a href="#" class="btn btn-primary" id="btn" data-toggle="modal" data-target="#stateModal" onClick=popup("'+trimmedState+'")>&#63;</a></div>')
             })
         }
-    })
+    })    
 })
--- server/2019.js
+++ server/2019.js
@@ -1,8 +1,35 @@
+// Function for adding content to the Modal popup
 function popup(state) {
+    
     newState = state.toLowerCase()
-    console.log(newState)
-    let popup = document.getElementById("statePopup")
-    popup.classList.toggle("show")
+
+    // Ajax query calling slug information from API and posting it onto popup
+    $(function () {
+
+        $.ajax({
+            type: 'GET',
+            url: 'https://datausa.io/api/profile?slug=geo&id='+ newState +'',
+            success: function(data) {
+
+                const $title = $('#stateModalTitle')
+                const $info = $('#info1')
+
+                // Populating content
+                $title.append(state)
+                $info.append(data.ids)
+            }
+        })
+    })
+}
+
+// Function that makes it so the information on the Modal popup is cleared
+function clearPopup() {
+    const $title = $('#stateModalTitle')
+    const $info = $('#info1') 
+
+    // Clears selected tag
+    $title.empty()
+    $info.empty()
 }
 
 
@@ -16,23 +43,14 @@ $(function () {
         type: 'GET',
         url: 'https://datausa.io/api/data?drilldowns=State&measures=Population&year=2019',
         success: function(data) {
-            data.preventDefault
-            // console.log(data.data)
-            $.each(data.data, function(i, stuff) {
-                stuff.preventDefault
-                let trimmedState = $.trim(stuff.State)
-                // console.log(stuff)
-                $stateCards.append('<div class="card" id="'+ stuff.State +'"><p class="card-year"> '+ stuff.State +'</p><p class="card-population">'+ stuff.Population +'<p><button id="btn" onClick=popup("'+trimmedState+'")>&#63;</button><span class="popuptext" id="statePopup">Wokring yo</span></div>')
+            $.each(data.data, function(i, stateData) {
+
+                const s = stateData
+                let trimmedState = $.trim(s.State)
+
+                // Formatting for the state/population cards, and includes the function calls
+                $stateCards.append('<div class="card" id="'+ s.State +'"><p class="card-year"> '+ s.State +'</p><p class="card-population">'+ s.Population +'<p><a href="#" class="btn btn-primary" id="btn" data-toggle="modal" data-target="#stateModal" onClick=popup("'+trimmedState+'")>&#63;</a></div>')
             })
         }
-    })
-
-    // $(document).ready(function(){
-    //     $('#btn').click(function() {
-    //         alert("this is where data goes")
-    //         console.log("working")
-    //     })
-    // })
-    
+    })    
 })
-
